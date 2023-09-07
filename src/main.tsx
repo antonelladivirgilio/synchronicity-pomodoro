cb79627f58bb579e47f0ba51b3b2cab46ffa2e54
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-07 16:42:56 -0300

    add app identifier prefix

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import {PomodoroProvider} from './contexts/pomodoroContext'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { PomodoroProvider } from "./contexts/pomodoroContext";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <PomodoroProvider>     
+ReactDOM.createRoot(document.getElementById("root")!, {
+  identifierPrefix: "app-pomodoro-",
+}).render(
+  <PomodoroProvider>
     <App />
   </PomodoroProvider>
-)
+);
