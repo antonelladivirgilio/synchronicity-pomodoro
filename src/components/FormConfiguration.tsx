7e8371c4f212a3edcd0f7fc18a7b167da8980ad1
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-07 19:38:26 -0300

    update FormConfiguration component to use Select component and update Pomodoro context

--- src/components/FormConfiguration.tsx
+++ src/components/FormConfiguration.tsx
@@ -1,17 +1,69 @@
-import { useId } from "react";
+import React, { useId, useState } from "react";
+import { BLOCKS_TIME, CICLE_STATES } from "../constants";
+import { type CiclePos } from "../types";
+import { usePomodoro } from "../hooks/usePomodoro";
+import { Select } from "./Select";
 
 export function FormConfiguration() {
   const inputId = useId();
+  const timeList = Array.from(Object.values(BLOCKS_TIME));
+
+  const { cicle, updateCicle } = usePomodoro();
+  const [timers, setTimers] = useState<CiclePos[]>(cicle);
+
+  const handleSelectChange = (
+    event: React.ChangeEvent<HTMLSelectElement>
+  ): void => {
+    const { name, value } = event.target;
+
+    const updatedValue = timers.map((current) => {
+      if (current.state === name) {
+        return {
+          ...current,
+          time: parseInt(value),
+        };
+      } else {
+        return current;
+      }
+    });
+
+    setTimers(updatedValue);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+    updateCicle({ cicle: timers });
+  };
+
   return (
-    <form>
-      <label>Pomodoro</label>
-      <input id={`pomodoro-${inputId}`}></input>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor={`pomodoro-${inputId}`}>Pomodoro</label>
 
-      <label>Short break</label>
-      <input id={`short-break-${inputId}`}></input>
+      <Select
+        options={timeList}
+        id={`pomodoro-${inputId}`}
+        onChange={handleSelectChange}
+        name={CICLE_STATES.POMODORO}
+        defaultValue={5}
+      />
 
-      <label>Long break</label>
-      <input id={`long-break-${inputId}`}></input>
+      <label htmlFor={`short-break-${inputId}`}>Short break</label>
+      <Select
+        options={timeList}
+        id={`short-break-${inputId}`}
+        onChange={handleSelectChange}
+        name={CICLE_STATES.SHORT_BREAK}
+        defaultValue={2}
+      />
+
+      <label htmlFor={`long-break-${inputId}`}>Long break</label>
+      <Select
+        options={timeList}
+        id={`long-break-${inputId}`}
+        onChange={handleSelectChange}
+        name={CICLE_STATES.LONG_BREAK}
+        defaultValue={2}
+      />
 
       <button>Guardar cambios</button>
     </form>
