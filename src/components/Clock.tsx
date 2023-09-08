535dc3898582c0ee4bd05ddfe54ba77086cf0739
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-08 19:10:20 -0300

    update Clock component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,12 +1,56 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { usePomodoro } from "../hooks/usePomodoro";
+import { millisecondsToMinutes } from "../utils/millisecondsToMinutes";
 
 export function Clock() {
   const { cicle } = usePomodoro();
+  const cicleCounter = useRef(0);
 
-  console.log({ cicle });
+  const currentCicle = cicle[cicleCounter.current]?.state;
+  const initialTime = cicle[cicleCounter.current]?.time;
 
-  const [time, setTime] = useState();
+  const [time, setTime] = useState(initialTime);
+  const [focusing, setFocusing] = useState(false);
 
-  return <h1>reloj: {JSON.stringify(cicle)}</h1>;
+  const handleFocusing = () => {
+    setFocusing(!focusing);
+  };
+
+  const handleReset = () => {
+    cicleCounter.current = 0;
+    setFocusing(false);
+    setTime(initialTime);
+  };
+
+  useEffect(() => {
+    if (time === 0) {
+      cicleCounter.current += 1;
+
+      if (cicleCounter.current > 5) {
+        handleReset();
+        return;
+      }
+
+      setTime(cicle[cicleCounter.current].time);
+      return;
+    }
+
+    const updateTime = setInterval(() => {
+      if (!focusing) return;
+
+      setTime((prev) => prev - 1000);
+    }, 1000);
+
+    return () => clearInterval(updateTime);
+  });
+
+  return (
+    <>
+      <h1>reloj: {millisecondsToMinutes(time)}</h1>
+      <h2>{currentCicle}</h2>
+
+      <button onClick={handleFocusing}>{focusing ? "Pausa" : "Empezar"}</button>
+      <button onClick={handleReset}>Reset Pomodoro</button>
+    </>
+  );
 }
