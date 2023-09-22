320040e0d04cdac0a85cda4e34a87eac440f3a7e
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-22 20:43:15 -0300

    format timer

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,6 @@
 import { Dispatch, SetStateAction, useEffect } from 'react'
 import { useTimer } from '../hooks/useTimer'
+import { millisecondsToMinutes } from '../utils/millisecondsToMinutes'
 
 type TimerProps = {
   stop: boolean
@@ -18,9 +19,10 @@ export function Timer({ stop }: TimerProps) {
     return () => clearInterval(interval)
   }, [time, stop])
 
+  const formattedTime = millisecondsToMinutes(time)
   return (
     <>
-      <h1 className="text-center text-7xl self-end mb-16">{time}</h1>
+      <h1 className="text-center text-7xl self-end mb-16">{formattedTime}</h1>
     </>
   )
 }
