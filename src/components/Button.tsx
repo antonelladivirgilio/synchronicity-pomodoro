728276db2e65c9e6d6d5e91e3781738ed18e377c
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-22 12:54:45 -0300

    refactor Button component to accept children

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,19 @@
 type ButtonProps = {
-    onClick: () => void;
-    text: string;
-    ariaLabel?: string
-}
+  onClick: () => void;
+  text?: string;
+  ariaLabel?: string;
+  children?: JSX.Element;
+};
 
-export function Button({onClick, text, ariaLabel}: ButtonProps) {
-
-    return <button onClick={onClick} aria-label={ariaLabel}>{text}</button>
+export function Button({ onClick, text, ariaLabel, children }: ButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      aria-label={ariaLabel}
+      className="rounded-md bg-lime-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-50"
+    >
+      {text && text}
+      {children && children}
+    </button>
+  );
 }
