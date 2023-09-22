847bdce2249643e6289ac420c8b092a342c0ce66
Antonella Di Virgilio <divirgilioantonella@gmail.com>
2023-09-22 12:56:08 -0300

    styles Modal component

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,39 +1,43 @@
-import { ReactNode } from "react";
-
 type ModalProps = {
-  title: string;
-  openModal: boolean;
-  children?: ReactNode;
-  primaryAction?: () => void;
-  secondaryAction?: () => void;
-};
+  title: string
+  openModal: boolean
+  children?: JSX.Element
+  primaryAction?: () => void
+  secondaryAction?: () => void
+}
 
 export function Modal({
   children,
   title,
   openModal,
   primaryAction,
-  secondaryAction,
+  secondaryAction
 }: ModalProps) {
   return (
     <>
       {openModal && (
-        <dialog style={{ display: openModal ? "block" : "none" }}>
-          <h1>{title}</h1>
-          {children && children}
+        <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto focus:outline-none">
+          <dialog
+            tabIndex={0}
+            style={{ display: openModal ? 'block' : 'none' }}
+            className="shadow-2xl rounded-md p-10"
+          >
+            <h1>{title}</h1>
+            {children && children}
 
-          {(primaryAction || secondaryAction) && (
-            <footer>
-              {primaryAction && (
-                <button onClick={primaryAction}>Guardar Cambios</button>
-              )}
-              {secondaryAction && (
-                <button onClick={secondaryAction}>Cancelar</button>
-              )}
-            </footer>
-          )}
-        </dialog>
+            {(primaryAction || secondaryAction) && (
+              <footer>
+                {primaryAction && (
+                  <button onClick={primaryAction}>Guardar Cambios</button>
+                )}
+                {secondaryAction && (
+                  <button onClick={secondaryAction}>Cancelar</button>
+                )}
+              </footer>
+            )}
+          </dialog>
+        </div>
       )}
     </>
-  );
+  )
 }
